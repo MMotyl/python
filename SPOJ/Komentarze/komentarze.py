bc88f9fc8ac739d55802aefd8c6f45778fadc104
Marcin Motyl <marcin.motyl@gmail.com>
2020-07-22 23:44:16 +0200

    kom

--- SPOJ/Komentarze/komentarze.py
+++ SPOJ/Komentarze/komentarze.py
@@ -6,22 +6,27 @@ def checkLine(line):
     global groupComment
     tmpComment = False
     line = line.replace('\n','')
-    if not groupComment and (line.find('/*')!= -1):
+    kon  = line.find('*/')
+    pocz = line.find('/*')
+
+    if (kon!= -1 ) and (pocz!= -1):
+        tmpComment = True
+        txt = line[0:pocz]+line[kon+2:len(line)]
+        if txt == '': txt =' '
+        print(txt.rstrip())
+    if not groupComment and not tmpComment and (pocz!= -1):
         groupComment = True
         print(line.split('/*')[0])
-    if groupComment & (line.find('*/')!= -1):
+    if groupComment & (kon!= -1):
         groupComment = False
         tmpComment = True        
-    if (line.find('*/')!= -1 ) and (line.find('/*')!= -1):
-        tmpComment = True
-        print(' ')
     if not (groupComment or tmpComment):
         print(line.split('//')[0])
 
 #for poz in fileinput.input():
 #    checkLine(line)
 
-path = 'D:\\Programowanie\\GIT_Python\\python\\SPOJ\\Komentarze\\dane2.txt'
+path = 'D:\\Programowanie\\GIT_Python\\python\\SPOJ\\Komentarze\\dane.txt'
 with open(path, encoding='UTF-8') as plik:
      while True:
         line = plik.readline()
